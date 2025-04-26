d1f41d3f13337bf7cb22a54ffc486296829232c0
abhiSahuCodes <abhishek963sahu@gmail.com>
2025-04-26 17:18:08 +0530

    Add update to getting book details using author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,8 +29,21 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+  const allKeys = Object.keys(books);
+
+  const bookByAuthor = allKeys
+    .filter((key) => books[key].author === author)
+    .reduce((acc, key) => {
+      acc[key] = books[key];
+      return acc;
+    }, {});
+
+  if (Object.keys(bookByAuthor).length > 0) {
+    res.status(200).json(bookByAuthor);
+  } else {
+    res.status(404).json({ message: `No books found for author: ${author}` });
+  }
 });
 
 // Get all books based on title
