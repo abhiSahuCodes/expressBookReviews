f0678bb6ef051a8ade8878e8951217ce8627f8dc
abhiSahuCodes <abhishek963sahu@gmail.com>
2025-04-26 17:25:06 +0530

    Add update to get book by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -48,8 +48,21 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  const allKeys = Object.keys(books);
+
+  const bookByTitle = allKeys
+   .filter((key) => books[key].title === title)
+   .reduce((acc, key) => {
+      acc[key] = books[key];
+      return acc;
+    }, {});
+
+  if (Object.keys(bookByTitle).length > 0) {
+    res.status(200).json(bookByTitle);
+  } else {
+    res.status(404).json({ message: `No books found for title: ${title}` });
+  }
 });
 
 //  Get book review
